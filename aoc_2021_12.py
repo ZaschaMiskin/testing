6cb490152061f149c9ffffb9eb9259eb6462f3ce
ZaschaMiskin <zascha.miskin@hotmail.com>
2021-12-14 10:09:30 +0100

    split 11,12, doc 14

--- aoc_2021_12.py
+++ aoc_2021_12.py
@@ -1,58 +1,3 @@
-grid = []
-flashes = 0
-
-
-def flash_adj(i, j, flashed):
-    global flashes
-    xrange = list(range(i - 1, i + 2))
-    yrange = list(range(j - 1, j + 2))
-    if -1 in xrange:
-        xrange.remove(-1)
-    if 10 in xrange:
-        xrange.remove(10)
-    if -1 in yrange:
-        yrange.remove(-1)
-    if 10 in yrange:
-        yrange.remove(10)
-    for x in xrange:
-        for y in yrange:
-            if (not (x == i and y == j)) and not flashed[x][y]:
-                grid[x][y] += 1
-                if grid[x][y] > 9:
-                    flashes += 1
-                    flashed[x][y] = True
-                    grid[x][y] = 0
-                    flash_adj(x, y, flashed)
-
-
-def day11():
-    # both part 1 and 2, just change return statement
-    global flashes
-    file = open("inputs/input11.txt")
-    for line in file:
-        grid.append(list(map(int, line.strip())))
-    for it in range(100000):
-        flashed = [[False for a in grid[0]] for b in grid]
-        for i, line in enumerate(grid):
-            for j, el in enumerate(line):
-                if not flashed[i][j]:
-                    grid[i][j] += 1
-                if grid[i][j] > 9:
-                    flashes += 1
-                    grid[i][j] = 0
-                    flashed[i][j] = True
-                    flash_adj(i, j, flashed)
-        for line in grid:
-            print(line)
-        out = True
-        for line in flashed:
-            for el in line:
-                if not el:
-                    out = False
-        if out:
-            return it + 1
-
-
 paths = []
 
 
@@ -100,4 +45,4 @@ def day12():
     return len(paths)
 
 
-print(day12())
+print(day11())
--- aoc_2021_14.py
+++ aoc_2021_14.py
@@ -10,6 +10,7 @@ def day14():
     op = ""
     conn = dict()
     occ = Counter()
+    iterations = 40  # stage 1: 10   stage 2:40
 
     for line in file:
         if line == "\n":
@@ -21,13 +22,15 @@ def day14():
     for l in range(len(op) - 1):
         viewed = op[l:l + 2]
         occ[viewed] += 1
-    for i in range(40):
+    for i in range(iterations):
         tmp = Counter()
         for el in occ:
             tmp[el[0] + conn[el]] += occ[el]
             tmp[conn[el] + el[1]] += occ[el]
             pass
         occ = tmp
+    # count first letter of every pattern + last letter of
+    # origin word(will be last letter of finished word) to get every letter
     c = Counter()
     for el in occ:
         c[el[0]] += occ[el]
